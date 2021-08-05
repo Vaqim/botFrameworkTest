b5d0042649258931f20bef4a30cb64471491a400
Vaqim <vadimser1355@gmail.com>
2021-08-05 17:31:13 +0300

    fix issue

--- dialogs/cardSchemas/invalidZipCode.js
+++ dialogs/cardSchemas/invalidZipCode.js
@@ -6,7 +6,7 @@ const card = CardFactory.heroCard(
   'Ooops..!',
   'It doesn`t seem valid zip code.\nYou can return to the main menu or try again.',
   null,
-  [(getPostBackButton('Try Again', 'findcafe'), getPostBackButton('Menu', 'menu'))],
+  [getPostBackButton('Try Again', 'findcafe'), getPostBackButton('Menu', 'menu')],
 );
 
 module.exports = card;
